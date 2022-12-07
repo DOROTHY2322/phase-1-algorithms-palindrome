3642e65cd6bb3c755fa643df99e5c00b76850209
DOROTHY2322 <kimelidorothy@gmail.com>
2022-12-07 19:35:51 +0300

    palindrome

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 function isPalindrome(word) { 
-  let polishedString = word.replace(/\w+|_/g,"");
-  let reversedstring = polishedString.split("").reverse().join("")
-  console.log(reversedstring, polishedString)
-  if(polishedString != reversedstring){
+  let String = word.replace();
+  let string = String.split("").reverse().join("")
+  console.log(string, String)
+  if(String !== string){
     return false;
   }
   return true;
@@ -12,10 +12,19 @@ let result = isPalindrome("robot")
 console.log(result)
 /* 
   Add your pseudocode here
+  should return boolean true or false if the word is palindrome
+create avariable called String
+return the string
+use conditional if to satisfy the conditions set
+use comparisons that is the strict inequality and loose inequality
+
 */
 
 /*
   Add written explanation of your solution here
+  a palindrome is a word or sentence thats spelled the same both forward and backword ignoring punctuation and case.
+The strict inequality operator returns true if two values are not equal without performing type conversions
+while the loose inequality returns true if the values are not equal.
 */
 
 // You can run `node index.js` to view these console logs
